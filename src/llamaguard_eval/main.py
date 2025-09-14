42de429d90d9b503d03fb87905c070cdd0fb6073
Drishti Sushma <129742046+DrishtiShrrrma@users.noreply.github.com>
2025-09-15 00:56:38 +0530

    Update main.py

--- src/llamaguard_eval/main.py
+++ src/llamaguard_eval/main.py
@@ -3,7 +3,11 @@ import json
 import pandas as pd
 from datasets import load_dataset as hf_load
 
-from src.authenticate_hf import authenticate
+try:
+    from src.authenticate_hf import authenticate
+except ImportError:
+    from authenticate_hf import authenticate
+
 from models import load_llama_guard, classify_instruction_answer
 from parser import parse_output
 from utils import save_results, normalize_dataset
